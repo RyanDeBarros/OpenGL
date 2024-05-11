56ef7a0181e5565a84c47593398755fe0a43fb42
RyanDeBarros <ryan2004db@gmail.com>
2024-05-10 22:59:43 -0400

    Basic vertex array object example.

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -67,7 +67,7 @@ static unsigned int compile_shader(unsigned int type, const std::string& source)
 {
 	GLCall(unsigned int id = glCreateShader(type));
 	const char* src = source.c_str();
-	GLCall(glShaderSource(id, 1, &src, NULL));
+	GLCall(glShaderSource(id, 1, &src, nullptr));
 	GLCall(glCompileShader(id));
 
 	int result;
@@ -111,8 +111,12 @@ int main()
 	if (!glfwInit())
 		return -1;
 
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
 	// Create a windowed mode window and its OpenGL context
-	GLFWwindow* window = glfwCreateWindow(1440, 1080, "Hello World!", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(1440, 1080, "Hello World!", nullptr, nullptr);
 	if (!window)
 	{
 		glfwTerminate();
@@ -139,10 +143,14 @@ int main()
 		2, 3, 0
 	};
 
+	unsigned int vao;
+	GLCall(glGenVertexArrays(1, &vao));
+	GLCall(glBindVertexArray(vao));
+	
 	unsigned int buffer;
 	GLCall(glGenBuffers(1, &buffer));
 	GLCall(glBindBuffer(GL_ARRAY_BUFFER, buffer));
-	GLCall(glBufferData(GL_ARRAY_BUFFER, 6 * 2 * sizeof(float), positions, GL_STATIC_DRAW));
+	GLCall(glBufferData(GL_ARRAY_BUFFER, 4 * 2 * sizeof(float), positions, GL_STATIC_DRAW));
 
 	GLCall(glEnableVertexAttribArray(0));
 	GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0));
@@ -150,7 +158,7 @@ int main()
 	unsigned int ibo;
 	GLCall(glGenBuffers(1, &ibo));
 	GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo));
-	GLCall(glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW));
+	GLCall(glBufferData(GL_ELEMENT_ARRAY_BUFFER, 3 * 2 * sizeof(unsigned int), indices, GL_STATIC_DRAW));
 
 	ShaderProgramSource source = parse_shader("res/shaders/Basic.shader");
 	unsigned int shader = create_shader(source.vertexSource, source.fragmentSource);
@@ -159,10 +167,15 @@ int main()
 	GLCall(int location = glGetUniformLocation(shader, "u_Color"));
 	ASSERT(location != -1);
 
+	GLCall(glBindVertexArray(0));
+	GLCall(glUseProgram(0));
+	GLCall(glBindBuffer(GL_ARRAY_BUFFER, 0));
+	GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0));
+
 	float r = 0.0f;
 	float increment = 0.05f;
 
-	const double fpsLimit = 1.0 / 60.0;
+	const double fpsLimit = 1.0 / 30.0;
 	double lastUpdateTime = 0;	// number of seconds since the last loop
 	double lastFrameTime = 0;	// number of seconds since the last frame
 
@@ -182,8 +195,12 @@ int main()
 			// Render here
 			GLCall(glClear(GL_COLOR_BUFFER_BIT));
 
+			GLCall(glUseProgram(shader));
+			GLCall(glBindVertexArray(vao));
+			GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo));
+
 			GLCall(glUniform4f(location, r, 0.3f, 0.8f, 1.0f));
-			GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, NULL));
+			GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
 
 			if (r > 1.0f)
 				increment = -0.05f;
